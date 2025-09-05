613844e718e2b9d625e73bdcf352a3c4030a3ff5
DanyRoland <eldanielitu@gmail.com>
2025-09-05 11:36:11 -0400

    procedimiento y funcion
    el procedimiento no retorna nada, una función sí retorna algo

--- code.kt
+++ code.kt
@@ -1,15 +1,15 @@
+fun saludar(nombre: String, saludo: String = "Hola"){
+    println(saludo + ", " + nombre)
+}
+
+fun generar(genero: String): String {
+    return "Mi género es ${genero}"
+}
+
 fun main(){
     val nombres: String = "Daniel Roland"
-    val apellidos: String = "Peñaranda Colque"
-    val materia: String = "Aplicaciones Móviles"
-    val peso: Double = 56.8 //Kg
-    val altura: Double = 1.72 //Mts
-    val edad: Int = 20
-    val esPar: Boolean = edad%2==0
     val generos: List<String> = listOf("Masculino", "Femenino", "Personalizado", "Prefiero no decirlo")
-        
-    println("Hola! Soy ${nombres + ' ' + apellidos}.")
-    println("Tengo ${edad} años ${if(esPar) "(par)" else "impar"}.")
-    println("Mi peso es de ${peso}Kg. y mido ${altura}Mts.")
-    println("Estoy estudiando ${materia} y mi género es ${generos[0]}.")
+    
+    saludar(nombres)
+    println(generar(generos[0]))
 }
