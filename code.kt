54015ac145b2380ced45426cfa704c55c4007bc9
DanyRoland <eldanielitu@gmail.com>
2025-09-10 15:38:51 -0400

    Tarea completada

--- code.kt
+++ code.kt
@@ -1,15 +1,46 @@
+enum class Genero(){
+    Male, Female, Custom, Hidden
+}
+
+data class Persona(
+    val nombres: String,
+    val peso: Double,
+    val altura: Double,
+    val edad: Int,
+    val genero: Genero
+){
+    fun saludar(){
+        println("Hola, soy ${nombres}!");
+    }
+    fun esMayor(){
+        if(this.edad>=18){
+            println("Soy mayor de edad");
+        }
+        else{
+            throw Error("YOUR ACCOUNT IS BEING TERMINATED");
+        }
+    }
+    fun generar(): String{
+        when(genero){
+            Genero.Male -> return "Género masculino";
+            Genero.Female -> return "Género femenino";
+            Genero.Custom -> return "Género personalizado";
+            Genero.Hidden -> return "Prefiero no decirlo";
+            // else -> return "Género desconocido";
+        }
+    }
+}
+
 fun main(){
-    val nombres: String = "Daniel Roland"
-    val apellidos: String = "Peñaranda Colque"
-    val materia: String = "Aplicaciones Móviles"
-    val peso: Double = 56.8 //Kg
-    val altura: Double = 1.72 //Mts
-    val edad: Int = 20
-    val esPar: Boolean = edad%2==0
-    val generos: List<String> = listOf("Masculino", "Femenino", "Personalizado", "Prefiero no decirlo")
-        
-    println("Hola! Soy ${nombres + ' ' + apellidos}.")
-    println("Tengo ${edad} años ${if(esPar) "(par)" else "impar"}.")
-    println("Mi peso es de ${peso}Kg. y mido ${altura}Mts.")
-    println("Estoy estudiando ${materia} y mi género es ${generos[0]}.")
+    val Yo = Persona(
+        nombres = "Daniel Penaranda",
+        peso = 56.8,
+        altura = 1.72,
+        edad = 20,
+        genero = Genero.Male
+    )
+
+    Yo.saludar();
+    Yo.esMayor();
+    println(Yo.generar());
 }
